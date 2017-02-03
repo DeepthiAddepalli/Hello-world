bd0cd43d8ea9e2978894e86234477c387dcfc219
Deepthi Addepalli <deepthimaheswari.addepalli@pcc.edu>
2017-02-03 11:52:35 -0800

    Added main method with print statement

--- src/Package1/helloWorld.java
+++ src/Package1/helloWorld.java
@@ -1,7 +1,15 @@
 package Package1;
 
 /**
- * Created by deepthi on 2/3/2017.
+ * Created by Deepthi M Addepalli on 2/3/2017.
  */
 public class helloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello-World");
+        System.out.print("My name is Deepthi Addepalli and ");
+        System.out.println("this is my first project uploaded to git");
+        System.out.println("--");
+    }
 }
