fa8aa46f9a5fe140948161d00a7e5e275d14eab8
Deepthi Addepalli <deepthimaheswari.addepalli@pcc.edu>
2017-02-03 11:56:49 -0800

    prints date

--- src/Package1/helloWorld.java
+++ src/Package1/helloWorld.java
@@ -3,13 +3,14 @@ package Package1;
 /**
  * Created by Deepthi M Addepalli on 2/3/2017.
  */
+import java.util.*;
 public class helloWorld {
 
     public static void main(String[] args)
     {
         System.out.println("Hello-World");
         System.out.print("My name is Deepthi Addepalli and ");
-        System.out.println("this is my first project uploaded to git");
+        System.out.println("this is my first project uploaded to git on "+ new Date());
         System.out.println("--");
     }
 }
